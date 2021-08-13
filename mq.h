fd39415e57fbcc590abb372d3a8a9fa1c9130832
asherlie <lieber.asher@gmail.com>
2021-08-13 13:32:10 -0400

    added mq.h

---
+++ mq.h
@@ -0,0 +1,17 @@
+#include <pthread.h>
+
+#include "packet.h"
+
+/* used to queue messages up for broadcast */
+struct mq_entry{
+    struct mq_entry* next;
+    struct new_beacon_packet packet;
+    //char msg[32];
+};
+
+struct mqueue{
+    struct mq_entry* first, * last;
+    pthread_mutex_t lock;
+};
+
+void init_mqueue(struct mqueue* mq);
