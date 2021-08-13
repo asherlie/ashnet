bb91058f5019173eb09bfbc38392ca720e9f6b6c
asherlie <lieber.asher@gmail.com>
2021-08-12 22:53:33 -0400

    magic header is now used to verify ashnet packets

--- sr.c
+++ sr.c
@@ -1,5 +1,6 @@
 #include <sys/socket.h>
 #include <stdio.h>
+#include <string.h>
 #include <netinet/if_ether.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
@@ -13,13 +14,17 @@ int main(){
     int sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL)), packet_len;
     /* if we're filtering properly, we can keep this buffer small */
     unsigned char* buffer = malloc(100000);
-    struct new_beacon_packet bp;
+    struct new_beacon_packet bp, ref_bp;
+
+    init_new_beacon_packet(&ref_bp);
 
     while(1){
         packet_len = recvfrom(sock, buffer, 100000, 0, &s_addr, &sa_len);
 
         if(packet_len == sizeof(struct new_beacon_packet)){
-            puts("GOT IT :)");
+            memcpy(&bp, buffer, sizeof(struct new_beacon_packet));
+            if(memcmp(bp.magic_hdr, ref_bp.magic_hdr, sizeof(bp.magic_hdr)))break;
+            printf("\"%s\"\n", bp.ssid);
         }
         /*if(protocol)*/
         /*if(strstr(buffer+56, "asher"))printf("protocol: %i\n", );*/
