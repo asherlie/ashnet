b557a02a1daf33a3253eed55beff0f2d35d737c6
asherlie <lieber.asher@gmail.com>
2021-08-12 21:04:10 -0400

    added code to craft beacon packets

---
+++ packet.c
@@ -0,0 +1,68 @@
+#include "packet.h"
+#include <string.h>
+
+void init_beacon_packet(struct beacon_packet* bp){
+    unsigned char rth[56] = 
+    /*'\x00', '\x00', '\x38', '\x00', '\x6f', '\x08', '\x00', '\xc0'*/
+
+
+                                                /* 08:11:96:99 is part of IP addr, is this dynamically set? */
+"\x00\x00\x38\x00\x6f\x08\x00\xc0\x01\x00\x00\x40\x08\x11\x96\x99" \
+"\x7b\xb1\x61\x0c\x00\x00\x00\x00\x10\x02\x6c\x09\x80\x04\xe8\xa6" \
+"\x00\x0b\x00\x10\x18\x00\x03\x00\x02\x00\x00\x4c\x00\x10\x18\x03" \
+"\x06\x00\xaa\xd7\xc8\x01\xf2\x56";
+
+    unsigned char mstr[4] = "\x80\x00\x00\x00";
+    unsigned char daddr[6] = "\xff\xff\xff\xff\xff\xff";
+    unsigned char mgck2[5] = "\x64\x00\x00\x00\x00";
+    unsigned char tag[4] = "\x00\x9f\xe0\x02";
+
+/*
+"\x08\x11\x96\x99\x37\x90\x08\x11\x96\x99\x37\x90\x00\x00" \
+"\x00\x00\x00\x00\x00\x00\x00\x00
+
+\x0b\x4a\x4b" \
+"\x4e\x45\x54\x3a\x4d\x45\x4c\x4c\x6f\xc1\xaa\xd7\xc7"
+*/
+
+
+
+    memcpy(bp->radiotap_hdr, rth, 56);
+    memcpy(bp->magic, mstr, 4);
+    memcpy(bp->dest_addr, daddr, 6);
+    memset(bp->zeroes, 0, 10);
+    memcpy(bp->more_magic, mgck2, 5);
+    /* maxlen for ssid == 32b */
+    bp->ssid_len = '\x20';
+    bp->ssid_len = '\x0b';
+    memcpy(bp->tag, tag, 4);
+}
+
+void init_new_beacon_packet(struct new_beacon_packet* bp){
+/*magic until 08,11*/ // (28 bytes of magic, src starts at 29)-1
+    unsigned char packet[] = 
+     {
+    '\x00', '\x00', '\x12', '\x00', '\x2e', '\x48', '\x00', '\x00', '\x2e', '\x02', '\x6c', '\x09', '\xa0', '\x00', '\xe5', '\x03',
+    '\x00', '\x00', '\x80', '\x00', '\x00', '\x00', '\xff', '\xff', '\xff', '\xff', '\xff', '\xff', '\x08', '\x11', '\x96', '\x99',
+    '\x37', '\x90', '\x08', '\x11', '\x96', '\x99', '\x37', '\x90', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00',
+    '\x00', '\x00', '\x64', '\x00', '\x00', '\x00', '\x00', '\x20', '\x42', '\x42', '\x42', '\x42', '\x42', '\x42', '\x42', '\x42', 
+    '\x42', '\x42', '\x42', '\x42', '\x42', '\x42', '\x42', '\x42', '\x42', '\x42', '\x42', '\x42', '\x42', '\x42', '\x42', '\x42', 
+    '\x42', '\x42', '\x42', '\x42', '\x42', '\x42', '\x42', '\x42', '\x6a', '\x00', '\x85', '\xc3'
+     };
+
+     memcpy(bp, packet, sizeof(struct new_beacon_packet));
+}
+
+void nbp_set_bssid(struct new_beacon_packet* bp, unsigned char* bssid){
+    memcpy(bp->src_bssid, bssid, 6);
+}
+
+void nbp_set_src_addr(struct new_beacon_packet* bp, unsigned char* src_addr){
+    memcpy(bp->src_bssid, src_addr, 6);
+}
+
+void set_src(struct beacon_packet* bp, unsigned char* src_addr){
+
+    memcpy(bp->src_addr, src_addr, 6);
+    memcpy(bp->src_bssid, src_addr, 6);
+}
---
+++ packet.h
@@ -0,0 +1,38 @@
+//#include 
+struct beacon_packet{
+    unsigned char radiotap_hdr[56];
+    unsigned char magic[4];
+    unsigned char dest_addr[6];
+    unsigned char src_addr[6];
+    unsigned char src_bssid[6];
+    /* sequence number, timestamp - 0s seem to work */
+    unsigned char zeroes[10];
+    /* interval, imm block ack, tag_no */
+    unsigned char more_magic[5];
+    unsigned char ssid_len;
+    //unsigned char ssid[32];
+    /* trying with 11 for now to test JKNET:MELLo */
+    unsigned char ssid[11];
+    /* this might differ based on length, not sure */
+    unsigned char tag[4];
+};
+
+/* sizeof(struct new_beacon_packet)-4 should be sent */
+struct new_beacon_packet{
+    unsigned char magic_hdr[28];
+    unsigned char src_addr[6];
+    unsigned char src_bssid[6];
+    /* last byte of this is length of ssid, which is always 32b */
+    unsigned char mid_magic[16];
+    unsigned char ssid[32];
+    /* this should not be included when sending, it's recalculated implicitly */
+    unsigned char end_magic[4];
+};
+
+/* sets all fields but nvm(ssid_len), ssid, src addrs*/
+void init_beacon_packet(struct beacon_packet* bp);
+void init_new_beacon_packet(struct new_beacon_packet* bp);
+void set_src(struct beacon_packet* bp, unsigned char* src_addr);
+
+void nbp_set_bssid(struct new_beacon_packet* bp, unsigned char* bssid);
+void nbp_set_src_addr(struct new_beacon_packet* bp, unsigned char* src_addr);
