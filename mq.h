76134bdef46a0176de828520aa7c0783a3e926a9
asherlie <lieber.asher@gmail.com>
2021-08-13 17:26:39 -0400

    updated struct mq_entry

--- mq.h
+++ mq.h
@@ -6,6 +6,14 @@
 struct mq_entry{
     struct mq_entry* next;
     struct new_beacon_packet* packet;
+    /* if this is set, the broadcast thread will overwrite 
+     * the BSSID and mac address fields with local addr
+     *
+     * this may be unwanted behavior at times - for example,
+     * when spreading a message that did not originate at
+     * a given node
+     */
+    _Bool overwrite_addr;
 };
 
 struct mqueue{
@@ -15,3 +23,7 @@ struct mqueue{
 };
 
 void init_mqueue(struct mqueue* mq);
+/* if overwrite_addr is not set, nbp's address fields should have already been set */
+void insert_mqueue(struct mqueue* mq, struct new_beacon_packet* nbp, _Bool overwrite_addr);
+//struct new_beacon_packet* pop_mqueue_blocking(struct mqueue* mq);
+struct mq_entry* pop_mqueue_blocking(struct mqueue* mq);
